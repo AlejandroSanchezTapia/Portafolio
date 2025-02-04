bd3208d7a686044552cb6e6751bc1bb542edea0b
Alejandro Tapia <alejandro_tapia@autotodo.com>
2025-02-04 12:43:32 -0600

    change index

--- assets/js/aboutme.js
+++ assets/js/aboutme.js
@@ -6,6 +6,35 @@ document.addEventListener("DOMContentLoaded",() => {
   const profilePicture = document.getElementById("profile_picture");
   const skillsContainer = document.getElementById("skillsContainer");
   const carouselContainer = document.getElementById("carouselContainer");
+  const aboutmeContainer = document.getElementById("aboutme");
+  const interestContainer = document.getElementById("interestContainer");
+  const resumeContainer = document.getElementById("resumeContainer");
+  const servicesContainer = document.getElementById("servicesContainer");
+  
+
+  const fetchAboutMe = async () => {
+    try {
+      const data = await fetchResources();
+      console.log("Datos de aboutme:", data);
+
+      if (data && data.length > 0) {
+        const profileData = data.find(p => p.name === "aboutmeContainer");
+        console.log("Datos del value de aboutmeContainer:", profileData.value);
+        if (profileData && profileData.value) {
+          aboutmeContainer.innerHTML = profileData.value;
+        }
+        else{
+          console.error("No se encontro el valor del aboutmeContainer");
+        }
+      }
+      else{
+        console.error("La respuesta de la API está vacía o no es válida.");
+      }
+    }
+    catch (error) {
+      console.error("Error al obtener los datos de la API:", error);
+   }
+  };
 
   const fetchProfilePicture = async () => {
     try {
@@ -51,6 +80,29 @@ document.addEventListener("DOMContentLoaded",() => {
    }
   };
 
+  const fetchInterestContainer = async () => {
+    try {
+      const data = await fetchResources();
+      console.log("Datos de la API:", data);
+      if (data && data.length > 0) {
+        const profileData = data.find(p => p.name === "interestContainer");
+        console.log("Datos del valñue de skills:", profileData.value);
+        if (profileData && profileData.value) {
+          interestContainer.innerHTML = profileData.value;
+        }
+        else{
+          console.error("No se encontro el valor de los interestContainer");
+        }
+      }
+      else{
+        console.error("La respuesta de la API está vacía o no es válida.");
+      }
+    }
+    catch (error) {
+      console.error("Error al obtener los datos de la API:", error);
+   }
+  };
+
   const fetchCarrousel = async () => {
     try {
       const data = await fetchResources();
@@ -75,7 +127,60 @@ document.addEventListener("DOMContentLoaded",() => {
    }
   };
 
+  const fetchResumeContainer = async () => {
+    try {
+      const data = await fetchResources();
+      console.log("Datos de la API:", data);
+
+      if (data && data.length > 0) {
+        const profileData = data.find(p => p.name === "resumeContainer");
+        console.log("Datos del value de resumeContainer:", profileData.value);
+        if (profileData && profileData.value) {
+          resumeContainer.innerHTML = profileData.value;
+        }
+        else{
+          console.error("No se encontro el valor del resumeContainer");
+        }
+      }
+      else{
+        console.error("La respuesta de la API está vacía o no es válida.");
+      }
+    }
+    catch (error) {
+      console.error("Error al obtener los datos de la API:", error);
+   }
+  };
+
+  const fetchServicesContainer = async () => {
+    try {
+      const data = await fetchResources();
+      console.log("Datos de la API:", data);
+
+      if (data && data.length > 0) {
+        const profileData = data.find(p => p.name === "servicesContainer");
+        console.log("Datos del value de servicesContainer:", profileData.value);
+        if (profileData && profileData.value) {
+          servicesContainer.innerHTML = profileData.value;
+        }
+        else{
+          console.error("No se encontro el valor del servicesContainer");
+        }
+      }
+      else{
+        console.error("La respuesta de la API está vacía o no es válida.");
+      }
+    }
+    catch (error) {
+      console.error("Error al obtener los datos de la API:", error);
+   }
+  };
+  
+
+  fetchAboutMe();
   fetchProfilePicture();
   fetchSpansSkills();
+  fetchInterestContainer();
   fetchCarrousel();
+  fetchResumeContainer();
+  fetchServicesContainer();
 });
